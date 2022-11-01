b91927ee05824e16b96b52bcf08a60d399bcf774
terquimocrio <diego_25mj@hotmail.com>
2022-11-01 01:12:22 -0700

    toggle projects

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,28 @@
+const buttonsDivs = {
+    "show-casino" : "project-casino",
+    "show-dugout" : "project-dugout"
+}
+
 const portfolioBtnsDiv = document.getElementById("portfolio-buttons-div")
 
 function makeSelected(button){
+    let id = button.id
+    let buttonsDivsCopy = {
+        ...buttonsDivs
+    }
     button.classList.add("selected")
     portfolioBtnsDiv.querySelectorAll("a").forEach(element => {
         if(element != button){
             element.classList.remove("selected")
         }
     })
+
+    document.getElementById(buttonsDivs[id]).classList.remove("d-none")
+    console.log(document.getElementById(buttonsDivs[id]))
+    delete buttonsDivsCopy[id]
+    console.log(buttonsDivsCopy)
+    for (const key in buttonsDivsCopy) {
+        document.getElementById(buttonsDivsCopy[key]).classList.add("d-none")
+    }
 }
+
