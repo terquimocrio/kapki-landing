4b9c3ce1d170ab66277b8eedc9ecb3bade6881c0
terquimocrio <diego_25mj@hotmail.com>
2022-11-01 22:31:16 -0700

    almost ready

--- static/js/main.js
+++ static/js/main.js
@@ -1,28 +1,26 @@
 const buttonsDivs = {
-    "show-casino" : "project-casino",
-    "show-dugout" : "project-dugout"
-}
+    "show-casino": "project-casino",
+    "show-dugout": "project-dugout",
+};
 
-const portfolioBtnsDiv = document.getElementById("portfolio-buttons-div")
+const portfolioBtnsDiv = document.getElementById("portfolio-buttons-div");
 
-function makeSelected(button){
-    let id = button.id
+function makeSelected(button) {
+    let id = button.id;
     let buttonsDivsCopy = {
-        ...buttonsDivs
-    }
-    button.classList.add("selected")
-    portfolioBtnsDiv.querySelectorAll("a").forEach(element => {
-        if(element != button){
-            element.classList.remove("selected")
+        ...buttonsDivs,
+    };
+    button.classList.add("selected");
+    portfolioBtnsDiv.querySelectorAll("a").forEach((element) => {
+        if (element != button) {
+            element.classList.remove("selected");
         }
-    })
+    });
 
-    document.getElementById(buttonsDivs[id]).classList.remove("d-none")
-    console.log(document.getElementById(buttonsDivs[id]))
-    delete buttonsDivsCopy[id]
-    console.log(buttonsDivsCopy)
+    document.getElementById(buttonsDivs[id]).classList.remove("d-none");
+    delete buttonsDivsCopy[id];
     for (const key in buttonsDivsCopy) {
-        document.getElementById(buttonsDivsCopy[key]).classList.add("d-none")
+        document.getElementById(buttonsDivsCopy[key]).classList.add("d-none");
     }
 }
 
